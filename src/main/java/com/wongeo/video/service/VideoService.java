755fd214ba17e2fa426786f17da40558593e4954
巫鸦 <29992379@qq.com>
2022-01-09 21:44:01 +0800

    update

--- src/main/java/com/wongeo/video/service/VideoService.java
+++ src/main/java/com/wongeo/video/service/VideoService.java
@@ -5,12 +5,17 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @Component
 public class VideoService {
+
+    public VideoService() throws UnknownHostException {
+    }
 
     //插入
     public int insertUrl(String name, String path, String url) {
@@ -22,13 +27,19 @@ public class VideoService {
         return 0;
     }
 
+    private InetAddress address = InetAddress.getLocalHost();
+
+    private String getUrl(String name) {
+        return "http://" + address.getHostAddress() + ":8080/files/" + name;
+    }
+
     //查询
     public List<Video> getVideos() {
         List<Video> videos = new ArrayList<>();
         File[] files = new File("/Users/feng/updateFiles/").listFiles();
         for (int i = 0; i < files.length; i++) {
             File file = files[i];
-            String url = "http://localhost:8080/files/" + file.getName();
+            String url = getUrl(file.getName());
             Video video = new Video(i, file.getName(), url);
             videos.add(video);
         }
